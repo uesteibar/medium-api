86bfc7908146cf90468a49f9b242d85178c33e71
uesteibar <uesteibar@gmail.com>
2015-04-11 15:12:48 +0200

    API test with mocha

--- test/mediumApiSpec.js
+++ test/mediumApiSpec.js
@@ -19,4 +19,30 @@ describe('Medium API', function () {
 
 	});
 
+	describe('GET /user/:username/info', function () {
+
+		it('should get the given user profile', function (done) {
+			request.get(base_url + 'user/uesteibar/info', function(err, res, body) {
+				if (err) return done(err);
+				assert.equal(res.statusCode, 200);
+				assert.equal(JSON.parse(body).username, 'uesteibar');
+				done();
+			});
+		});
+
+	});
+
+	describe('GET /posts/:tag/:limit', function () {
+
+		it('should get the given user profile', function (done) {
+			request.get(base_url + 'posts/node/3', function(err, res, body) {
+				if (err) return done(err);
+				assert.equal(res.statusCode, 200);
+				assert.equal(JSON.parse(body).length, 3);
+				done();
+			});
+		});
+
+	});
+
 });
